12add21034f06085b658c3e3c6cb86b049e301a1
shovon99 <hasib99@live.com>
2017-12-02 21:36:47 +0600

    Icon Added

--- app/src/main/java/com/hasib/drawer/navigationdrawermikepenz/Welcome.java
+++ app/src/main/java/com/hasib/drawer/navigationdrawermikepenz/Welcome.java
@@ -1,12 +1,20 @@
 package com.hasib.drawer.navigationdrawermikepenz;
 
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.iconics.context.IconicsContextWrapper;
 
 
 public class Welcome extends Fragment {
@@ -21,7 +29,17 @@ public class Welcome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_welcome, container, false);
+
+        //TextView welcome = view.findViewById(R.id.textView9);
+        //Drawable dr = new IconicsDrawable(getActivity().getApplicationContext()).icon(FontAwesome.Icon.faw_briefcase).color(Color.RED);
+        //welcome.setCompoundDrawablesWithIntrinsicBounds(getResources().get,0,0,0);
+
+
+
+        return view;
     }
+
 
 }
